6aae19237c8d4638495ffe4e8f61b1714b00c635
Jorn Luiten <js.luiten@gmail.com>
2018-01-28 12:40:39 +0100

    Cleanup logs

--- src/GameBindings/bindingSelectors.js
+++ src/GameBindings/bindingSelectors.js
@@ -2,29 +2,22 @@ import { createSelector } from 'reselect'
 import { getPressedKey } from 'src/Keyboard/keyboardSelectors'
 
 const getBindings = (state) => {
-  let bindings = (state.bindings.gameData) ? state.bindings.gameData.bindings : {}
-  console.log('- getBindings')
-  return bindings
+  return (state.bindings.gameData) ? state.bindings.gameData.bindings : {}
 }
 
 export const filterSelectedBinding = (bindings) => {
   const b = Object.keys(bindings).find((b) => bindings[b].selected)
-  console.log('- - filterSelectedBinding')
   return bindings[b]
 }
 
 export const getSelectedBinding = createSelector(
   [ getBindings ],
-  (bindings) => {
-    console.log('- - - getSelectedBinding')
-    return filterSelectedBinding(bindings)
-  }
+  (bindings) => filterSelectedBinding(bindings)
 )
 
 export const getBindingAndKeyMatch = createSelector(
   [ getPressedKey, getSelectedBinding ],
   (key, binding) => {
-    console.log('getBindingAndKeyMatch')
     return (binding && key) ? { binding, key } : null
   }
 )
