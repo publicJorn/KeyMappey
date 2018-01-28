d8a89af74fd11fc2357a907ca766d3beef0e2db9
Jorn Luiten <js.luiten@gmail.com>
2018-01-28 15:17:10 +0100

    Ignore match if key is already bound

--- src/GameBindings/bindingSelectors.js
+++ src/GameBindings/bindingSelectors.js
@@ -5,6 +5,11 @@ const getBindings = (state) => {
   return (state.bindings.gameData) ? state.bindings.gameData.bindings : {}
 }
 
+const filterByBoundKeyLabel = (bindings, keyLabel) => {
+  const b = Object.keys(bindings).find((b) => bindings[b].boundKey === keyLabel)
+  return bindings[b]
+}
+
 export const filterSelectedBinding = (bindings) => {
   const b = Object.keys(bindings).find((b) => bindings[b].selected)
   return bindings[b]
@@ -16,8 +21,13 @@ export const getSelectedBinding = createSelector(
 )
 
 export const getBindingAndKeyMatch = createSelector(
-  [ getPressedKey, getSelectedBinding ],
-  (key, binding) => {
-    return (binding && key) ? { binding, key } : null
+  [ getBindings, getPressedKey, getSelectedBinding ],
+  (allBindings, key, binding) => {
+    // TODO: After MVP show a nice warning for the user if key was already bound
+    if (binding && key && !filterByBoundKeyLabel(allBindings, key.label)) {
+      return { binding, key }
+    }
+
+    return null
   }
 )
